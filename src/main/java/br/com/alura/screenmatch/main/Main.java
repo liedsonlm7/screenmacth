848b8a9455b6befaefa86b4df3cfee6d5e75b578
liedsonlm7 <liedsonlm7@gmail.com>
2025-09-02 11:12:00 -0300

    other statistics

--- src/main/java/br/com/alura/screenmatch/main/Main.java
+++ src/main/java/br/com/alura/screenmatch/main/Main.java
@@ -100,6 +100,13 @@ public class Main {
 
         System.out.println(avaliacoesPorTemporada);
 
+        DoubleSummaryStatistics est = episodios.stream().
+            filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.summarizingDouble(Episode::getAvaliacao));
+        System.out.println("Média: " + est.getAverage());
+        System.out.println("Melhor episódio: " + est.getMax());
+        System.out.println("Pior episódio: " + est.getMin());
+        System.out.println("Total de episódios avaliados: " + est.getCount());
 
     }
 }
