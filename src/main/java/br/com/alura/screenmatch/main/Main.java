0331b5e5f8f1e460b1de764edb039d0a4e99c89b
liedsonlm7 <liedsonlm7@gmail.com>
2025-09-03 09:23:57 -0300

    create model Serie and enum to list categories

--- src/main/java/br/com/alura/screenmatch/main/Main.java
+++ src/main/java/br/com/alura/screenmatch/main/Main.java
@@ -1,13 +1,16 @@
 package br.com.alura.screenmatch.main;
 
 import br.com.alura.screenmatch.model.SeasonData;
+import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.model.SeriesData;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConvertData;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -16,37 +19,47 @@ public class Main {
     private ConvertData convert = new ConvertData();
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=339000c9";
+    private List<SeriesData> seriesData = new ArrayList<>();
 
     public void exibeMenu() {
-        var menu = """
+        var opcao = -1;
+        while (opcao != 0) {
+            var menu = """
                 1 - Buscar séries
                 2 - Buscar episódios
+                3 - Listar séries buscadas
                 
                 0 - Sair
                 """;
 
-        System.out.println(menu);
-        System.out.println("Digite a opção desejada: ");
-        var opcao = sc.nextInt();
-        sc.nextLine();
+            System.out.println(menu);
+            System.out.println("Digite a opção desejada: ");
+            opcao = sc.nextInt();
+            sc.nextLine();
 
-        switch (opcao) {
-            case 1:
-                buscarSeriesWeb();
-                break;
-            case 2:
-                buscarEpisodioPorSerie();
-                break;
-            case 0:
-                System.out.println("Saindo...");
-                break;
-            default:
-                System.out.println("Opção inválida");
+            switch (opcao) {
+                case 1:
+                    buscarSeriesWeb();
+                    break;
+                case 2:
+                    buscarEpisodioPorSerie();
+                    break;
+                case 3:
+                    listarSeriesBuscadas();
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+            }
         }
+
     }
 
     private void buscarSeriesWeb() {
         SeriesData data = getSeriesData();
+        seriesData.add(data);
         System.out.println(data);
     }
 
@@ -71,6 +84,12 @@ public class Main {
         }
         temporadas.forEach(System.out::println);
 
+    }
+
+    private void listarSeriesBuscadas() {
+        List<Serie> series = new ArrayList<>();
+        series = seriesData.stream().map(d -> new Serie(d)).collect(Collectors.toList());
+        series.stream().sorted(Comparator.comparing(Serie::getGenero)).forEach(System.out::println);
     }
 }
 
---
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -0,0 +1,90 @@
+package br.com.alura.screenmatch.model;
+
+import java.util.OptionalDouble;
+
+public class Serie {
+    private String titulo;
+    private Integer totalTemporadas;
+    private double avaliacao;
+    private Categoria genero;
+    private String sinopse;
+    private String poster;
+    private String atores;
+
+    public Serie(SeriesData seriesData) {
+        this.titulo = seriesData.titulo();
+        this.totalTemporadas = seriesData.totalTemporadas();
+        this.avaliacao = OptionalDouble.of(Double.valueOf(seriesData.avaliacao())).orElse(0);
+        this.genero = Categoria.fromString(seriesData.genero().split(",")[0].trim());
+        this.atores = seriesData.atores();
+        this.poster = seriesData.poster();
+        this.sinopse = seriesData.sinopse();
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getTotalTemporadas() {
+        return totalTemporadas;
+    }
+
+    public void setTotalTemporadas(Integer totalTemporadas) {
+        this.totalTemporadas = totalTemporadas;
+    }
+
+    public double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public Categoria getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Categoria genero) {
+        this.genero = genero;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getAtores() {
+        return atores;
+    }
+
+    public void setAtores(String atores) {
+        this.atores = atores;
+    }
+
+    @Override
+    public String toString() {
+        return  "Gênero: " + genero + '\n' +
+                "Nome: " + titulo + '\n' +
+                "Temporadas: " + totalTemporadas + '\n' +
+                "Avaliação: " + avaliacao + '\n' +
+                "Sinopse: " + sinopse + '\n' +
+                "Poster: " + poster + '\n' +
+                "Atores: " + atores;
+    }
+}
