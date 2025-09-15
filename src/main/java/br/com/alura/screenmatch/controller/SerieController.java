d2224fb7b8c7fcd979c8b2b549f9cae9084ffd65
liedsonlm7 <liedsonlm7@gmail.com>
2025-09-15 18:32:44 -0300

    create services to manage series

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,27 +1,27 @@
 package br.com.alura.screenmatch.controller;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
-import br.com.alura.screenmatch.model.Serie;
-import br.com.alura.screenmatch.repository.SerieRepository;
+import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
 
     @Autowired
-    private SerieRepository repository;
+    private SerieService service;
 
     @GetMapping("/series")
     public List<SerieDTO> getSeries() {
-        return repository.findAll()
-                .stream()
-                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
-                .collect(Collectors.toList());
+        return service.getAllSeries();
+    }
+
+    @GetMapping("/series/top5")
+    public List<SerieDTO> getTop5Series() {
+        return service.getTop5Series();
     }
 
 }
